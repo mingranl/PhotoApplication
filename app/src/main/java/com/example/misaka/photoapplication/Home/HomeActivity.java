0eda17c531e597491fa7f676d7caf20104f84638
Feng Zhao <w441@me.com>
2018-10-18 23:00:07 +1100

    Completed login and register
    Completed login and register methods

--- app/src/main/java/com/example/misaka/photoapplication/Home/HomeActivity.java
+++ app/src/main/java/com/example/misaka/photoapplication/Home/HomeActivity.java
@@ -1,4 +1,6 @@
 package com.example.misaka.photoapplication.Home;
 
-public class HomeActivity {
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
 }
--- app/src/main/java/com/example/misaka/photoapplication/Login/RegisterActivity.java
+++ app/src/main/java/com/example/misaka/photoapplication/Login/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.misaka.photoapplication.Login;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,14 +12,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
 
 import com.example.misaka.photoapplication.R;
 import com.example.misaka.photoapplication.Home.HomeActivity;
+import com.example.misaka.photoapplication.Model.User;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,7 +37,6 @@ public class RegisterActivity extends AppCompatActivity {
     private Context mContext;
     private String email, username, password;
     private EditText mEmail, mPassword, mUsername;
-    private TextView loadingPleaseWait;
     private Button btnRegister;
     private Button btnBack;
     private ProgressBar mProgressBar;
@@ -58,6 +67,8 @@ public class RegisterActivity extends AppCompatActivity {
         mProgressBar.setVisibility(View.GONE);
 
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
 
         init();
 //        setupFirebaseAuth();
@@ -73,7 +84,113 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void init(){
-        
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmail.getText().toString();
+                username = mUsername.getText().toString();
+                password = mPassword.getText().toString();
+
+                if(checkInputs(email, username, password)){
+                    mProgressBar.setVisibility(View.VISIBLE);
+
+                    register(email, password);
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Back to login");
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean checkInputs(String email, String username, String password){
+        Log.d(TAG, "checkInputs: checking inputs for null values.");
+        if(email.equals("") || username.equals("") || password.equals("")){
+            Toast.makeText(mContext, "All fields must be filled out.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    // check if the username exists
+    // if yes, add random postfix to it then write all info to databse
+    private void checkUsername(final String userID, final String email, final String username){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child("users")
+                .orderByChild("username")
+                .equalTo(username);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    if (singleSnapshot.exists()) {
+                        Log.d(TAG, "checkIfUsernameExists: FOUND A MATCH: " + singleSnapshot.getValue(User.class).getUsername());
+                        append = myRef.push().getKey().substring(3, 10);
+                        Log.d(TAG, "onDataChange: username already exists. Appending random string to name: " + append);
+                    }
+                }
+
+                String mUsername = "";
+                mUsername = username + append;
+                writeNewUser(userID, mUsername, email);
+
+                Toast.makeText(mContext, "Signup successful.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void register(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            writeData(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            writeData(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    // write new user to users
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(userId, name, email);
+
+        myRef.child("users").child(userId).setValue(user);
+    }
+
+    // get current user to successive actions
+    private void writeData(FirebaseUser user){
+        if(user!=null){
+            Log.d(TAG, "Sign up: success.");
+            checkUsername(user.getUid(),user.getEmail(),username);
+            updateUI(user);
+        }
     }
 
     // if user has signed in then redirect to home page

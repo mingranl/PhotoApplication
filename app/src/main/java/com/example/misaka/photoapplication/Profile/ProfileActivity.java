11992870751c902ff4bcae919b68186e951386da
mingranl <mingranl@student.unimelb.edu.au>
2018-10-19 20:04:47 +1100

    update profile activity, remove useless layout

--- app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileActivity.java
@@ -1,5 +1,8 @@
 package com.example.misaka.photoapplication.Profile;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +14,23 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        init();
+    }
+
+    // initialize Profile activity
+    private void init(){
+        ProfileFragment fragment = new ProfileFragment();
+        String str = "Profile";
+        fragmentTransaction(fragment, str);
+    }
+
+
+    // function to manage transaction of fragments
+    public void fragmentTransaction (Fragment fragment, String str) {
+        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(str);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileFragment.java
@@ -13,12 +13,12 @@ import com.example.misaka.photoapplication.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link EditProfileFragment.OnFragmentInteractionListener} interface
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link EditProfileFragment#newInstance} factory method to
+ * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class EditProfileFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public EditProfileFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class EditProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfileFragment.
+     * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EditProfileFragment newInstance(String param1, String param2) {
-        EditProfileFragment fragment = new EditProfileFragment();
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class EditProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event

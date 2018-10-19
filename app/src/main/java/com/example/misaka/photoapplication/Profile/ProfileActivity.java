2bbbd4ec0c43c2ee1ebd96428e0d7acde737f5c7
mingranl <mingranl@student.unimelb.edu.au>
2018-10-19 20:32:02 +1100

    update Profile Activity

--- app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileActivity.java
@@ -5,24 +5,34 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.misaka.photoapplication.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+                Log.d(TAG, "onCreate: started.");
 
         init();
     }
 
     // initialize Profile activity
     private void init(){
+
         ProfileFragment fragment = new ProfileFragment();
         String str = "Profile";
-        fragmentTransaction(fragment, str);
+        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(str);
+        transaction.commit();
+
+
+        //fragmentTransaction(fragment, str);
     }
 
 

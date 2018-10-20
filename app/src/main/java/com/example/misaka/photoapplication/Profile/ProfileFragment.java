4d038806a19799a40ca4255daa533d8322143748
mingranl <mingranl@student.unimelb.edu.au>
2018-10-20 15:05:29 +1100

    add log out function. Change Profile files.

--- app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/misaka/photoapplication/Profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.misaka.photoapplication.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,13 +11,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.misaka.photoapplication.Login.LoginActivity;
 import com.example.misaka.photoapplication.R;
 import com.example.misaka.photoapplication.Util.NavigationBarActivate;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class ProfileFragment extends Fragment {
@@ -60,8 +67,91 @@ public class ProfileFragment extends Fragment {
 
         setupBottomNavigationView();
 
+//        getFollowersCount();
+//        getFollowingCount();
+//        getPostsCount();
+
+        // log out
+        Button logOut = (Button) view.findViewById(R.id.logout_btn);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
+
+//    //get number of followers
+//    private void getFollowersCount(){
+//        accFollowersNum = 0;
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//        Query query = reference.child(getString(R.string.dbname_followers))
+//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+//        query.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+//                    Log.d(TAG, "onDataChange: found follower:" + singleSnapshot.getValue());
+//                    accFollowersNum++;
+//                }
+//                accFollowers.setText(String.valueOf(accFollowersNum));
+//            }
+//        });
+//    }
+//
+//
+//    //get number of following
+//    private void getFollowingCount(){
+//        accFollowingNum = 0;
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//        Query query = reference.child(getString(R.string.dbname_following))
+//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+//        query.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+//                    Log.d(TAG, "onDataChange: found following user:" + singleSnapshot.getValue());
+//                    accFollowingNum++;
+//                }
+//                accFollowing.setText(String.valueOf(accFollowingNum));
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+//
+//    //get number of posts
+//    private void getPostsCount(){
+//        accPostsNum = 0;
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//        Query query = reference.child(getString(R.string.dbname_user_photos))
+//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+//        query.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+//                    Log.d(TAG, "onDataChange: found post:" + singleSnapshot.getValue());
+//                    accPostsNum++;
+//                }
+//                accPosts.setText(String.valueOf(accPostsNum));
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
     /**
      * BottomNavigationView
